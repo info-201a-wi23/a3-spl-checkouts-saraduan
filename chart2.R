406ea284a6c49e76d5e7a6d1d7ba2022e478ab29
Sara Duan <121904812+saraduan@users.noreply.github.com>
2023-03-07 05:43:24 -0800

    Add files via upload

---
+++ chart2.R
@@ -0,0 +1,37 @@
+# A3: chart 2
+# Sara Duan
+
+library("tidyverse")
+library("ggplot2")
+library("dplyr")
+library("scales")
+
+# load 2022-2023 checkouts dataset
+checkouts <- read.csv("/Users/sarajduan/Desktop/2017-2023-10-Checkouts-SPL-Data.csv",stringsAsFactors = FALSE)
+
+
+# checkout types
+checkout_types <- checkouts %>%
+  filter(UsageClass %in% c("Physical", "Digital")) %>%
+  group_by(CheckoutYear, UsageClass) %>%
+  summarise(total_checkouts = sum(Checkouts))
+
+
+# plot
+ggplot(data = checkout_types) + 
+  geom_bar(aes(
+    x = CheckoutYear, 
+    y = total_checkouts, 
+    fill = UsageClass),
+    stat = "identity"
+  ) +
+  scale_fill_manual(values = c("#59a5d8", "#386fa4")) +
+  scale_y_continuous(labels = label_number_si()) +
+  labs(
+    title = "Total Physical vs. Digital Checkouts Per Year (2017-2023)", 
+    x="Year", 
+    y="Number of Checkouts", 
+    fill="Book Type"
+  ) +
+  theme(axis.line = element_line(color = "black"),
+        plot.title = element_text(hjust = 0.5))
---
+++ chart3.R
@@ -0,0 +1,76 @@
+# A3: chart 3
+# Sara Duan
+
+library("tidyverse")
+library("ggplot2")
+library("dplyr")
+library("scales")
+
+# load 2017-2023 checkouts dataset
+checkouts <- read.csv("/Users/sarajduan/Desktop/2017-2023-10-Checkouts-SPL-Data.csv",stringsAsFactors = FALSE)
+
+
+# genres
+thriller_checkouts <- checkouts %>%
+  group_by(Subjects) %>%
+  filter(grepl('Thriller', Subjects)) %>%
+  summarise(total_checkouts = sum(Checkouts))
+total_thriller_checkouts <- sum(thriller_checkouts$total_checkouts)
+
+mystery_checkouts <- checkouts %>%
+  group_by(Subjects) %>%
+  filter(grepl('Mystery', Subjects)) %>%
+  summarise(total_checkouts = sum(Checkouts))
+total_mystery_checkouts <- sum(mystery_checkouts$total_checkouts)
+
+romance_checkouts <- checkouts %>%
+  group_by(Subjects) %>%
+  filter(grepl('Romance', Subjects)) %>%
+  summarise(total_checkouts = sum(Checkouts))
+total_romance_checkouts <- sum(romance_checkouts$total_checkouts)
+
+fantasy_checkouts <- checkouts %>%
+  group_by(Subjects) %>%
+  filter(grepl('Fantasy', Subjects)) %>%
+  summarise(total_checkouts = sum(Checkouts))
+total_fantasy_checkouts <- sum(fantasy_checkouts$total_checkouts)
+
+drama_checkouts <- checkouts %>%
+  group_by(Subjects) %>%
+  filter(grepl('Drama', Subjects)) %>%
+  summarise(total_checkouts = sum(Checkouts))
+total_drama_checkouts <- sum(drama_checkouts$total_checkouts)
+
+science_checkouts <- checkouts %>%
+  group_by(Subjects) %>%
+  filter(grepl('Science', Subjects)) %>%
+  summarise(total_checkouts = sum(Checkouts))
+total_science_checkouts <- sum(science_checkouts$total_checkouts)
+
+
+# plot
+genre_checkouts <- data.frame(
+  genre=c("Thriller", "Mystery", "Romance", 
+          "Fantasy", "Drama", "Science"),
+  total_checkouts=c(
+    total_thriller_checkouts, total_mystery_checkouts, 
+    total_romance_checkouts, total_fantasy_checkouts, 
+    total_drama_checkouts, total_science_checkouts))
+
+ggplot(data = genre_checkouts) +
+  geom_bar(aes(
+    x = genre,
+    y = total_checkouts,
+    fill = genre),
+    stat = "identity"
+  ) +
+  scale_fill_manual(values = c("#91e5f6", "#84d2f6", "#59a5d8", "#386fa4", "#133c55", "#051923")) +
+  scale_y_continuous(labels = label_number_si()) +
+  labs(
+    title = "Total Checkouts for Common Genres (2017-2023)", 
+    x = "Genre", 
+    y = "Number of Checkouts"
+  ) +
+  coord_flip() +
+  theme(axis.line = element_line(color = "black"),
+        plot.title = element_text(hjust = 0.5))
